d6ec4f32a9cc5fae639d4bc5a13c7d0091d8bddd
Iagodizlla <iagoschlemper@gmail.com>
2025-08-22 13:58:50 -0300

    Implementado Testes de Sala em Integracao

---
+++ ControleDeCinema.Testes.Integracao/ModuloSala/RepositorioSalaEmOrmTests.cs
@@ -0,0 +1,90 @@
+﻿using ControleDeCinema.Dominio.ModuloGeneroFilme;
+using ControleDeCinema.Dominio.ModuloSala;
+using TesteFacil.Testes.Integracao.Compartilhado;
+
+namespace ControleDeCinema.Testes.Integracao.ModuloSala;
+
+[TestClass]
+[TestCategory("Testes de Integração de Sala")]
+public sealed class RepositorioSalaEmOrmTests : TestFixture
+{
+    [TestMethod]
+    public void Deve_Cadastrar_Sala_Corretamente()
+    {
+        // Arrange
+        var sala = new Sala(001, 200);
+
+        // Act
+        repositorioSala?.Cadastrar(sala);
+        dbContext?.SaveChanges();
+
+        // Assert
+        var registroSelecionado = repositorioSala?.SelecionarRegistroPorId(sala.Id);
+
+        Assert.AreEqual(sala, registroSelecionado);
+    }
+
+    [TestMethod]
+    public void Deve_Editar_Sala_Corretamente()
+    {
+        // Arrange
+        var sala = new Sala(001, 200);
+        repositorioSala?.Cadastrar(sala);
+        dbContext?.SaveChanges();
+
+        var salaEditada = new Sala(002, 150);
+
+        // Act
+        var conseguiuEditar = repositorioSala?.Editar(sala.Id, salaEditada);
+        dbContext?.SaveChanges();
+
+        // Assert
+        var registroSelecionado = repositorioSala?.SelecionarRegistroPorId(sala.Id);
+
+        Assert.IsTrue(conseguiuEditar);
+        Assert.AreEqual(sala, registroSelecionado);
+    }
+
+    [TestMethod]
+    public void Deve_Excluir_Sala_Corretamente()
+    {
+        // Arrange
+        var sala = new Sala(001, 200);
+        repositorioSala?.Cadastrar(sala);
+        dbContext?.SaveChanges();
+
+        // Act
+        var conseguiuExcluir = repositorioSala?.Excluir(sala.Id);
+        dbContext?.SaveChanges();
+
+        // Assert
+        var registroSelecionado = repositorioSala?.SelecionarRegistroPorId(sala.Id);
+
+        Assert.IsTrue(conseguiuExcluir);
+        Assert.IsNull(registroSelecionado);
+    }
+
+    [TestMethod]
+    public void Deve_Selecionar_Sala_Corretamente()
+    {
+        // Arrange - Arranjo
+        var sala = new Sala(001, 200);
+        var sala2 = new Sala(002, 150);
+        var sala3 = new Sala(003, 220);
+
+        List<Sala> salasEsperados = [sala, sala2, sala3];
+
+        repositorioSala?.CadastrarEntidades(salasEsperados);
+        dbContext?.SaveChanges();
+
+        var salaEsperadasOrdenadas = salasEsperados
+            .OrderBy(d => d.Numero)
+            .ToList();
+
+        // Act - Ação
+        var salasRecebidas = repositorioSala?.SelecionarRegistros();
+
+        // Assert - Asseção
+        CollectionAssert.AreEqual(salaEsperadasOrdenadas, salasRecebidas);
+    }
+}
