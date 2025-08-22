c8ea21ba69a4aca75bf41ac04fa192096b0c1568
Iagodizlla <iagoschlemper@gmail.com>
2025-08-22 14:31:56 -0300

    Implementado Testes de Genero de Filme em Interface

---
+++ ControleDeCinema.Testes.Interface/ModuloGeneroFilme/GeneroFilmeInterfaceTests.cs
@@ -0,0 +1,68 @@
+﻿using TesteFacil.Testes.Interface.Compartilhado;
+
+namespace ControleDeCinema.Testes.Interface.ModuloGeneroFilme;
+
+[TestClass]
+[TestCategory("Tests de Interface de Genero de Filme")]
+public sealed class GeneroFilmeInterfaceTests : TestFixture
+{
+    [TestMethod]
+    public void Deve_Cadastrar_Genero_Filme_Corretamente()
+    {
+        // Arange
+        var indexPageObject = new GeneroFilmeIndexPageObject(driver!)
+            .IrPara(enderecoBase!);
+
+        // Act
+        indexPageObject
+            .ClickCadastrar()
+            .PreencherNome("Suspense")
+            .Confirmar();
+
+        // Assert
+        Assert.IsTrue(indexPageObject.ContemGeneroFilme("Suspense"));
+    }
+
+    [TestMethod]
+    public void Deve_Editar_Genero_Filme_Corretamente()
+    {
+        // Arrange
+        var indexPageObject = new GeneroFilmeIndexPageObject(driver!)
+            .IrPara(enderecoBase!);
+
+        indexPageObject
+            .ClickCadastrar()
+            .PreencherNome("Suspense")
+            .Confirmar();
+
+        // Act
+        indexPageObject
+            .ClickEditar()
+            .PreencherNome("Suspense Editada")
+            .Confirmar();
+
+        // Assert
+        Assert.IsTrue(indexPageObject.ContemGeneroFilme("Suspense Editada"));
+    }
+
+    [TestMethod]
+    public void Deve_Excluir_Genero_Filme_Corretamente()
+    {
+        // Arrange
+        var indexPageObject = new GeneroFilmeIndexPageObject(driver!)
+            .IrPara(enderecoBase!);
+
+        indexPageObject
+            .ClickCadastrar()
+            .PreencherNome("Suspense")
+            .Confirmar();
+
+        // Act
+        indexPageObject
+            .ClickExcluir()
+            .Confirmar();
+
+        // Assert
+        Assert.IsFalse(indexPageObject.ContemGeneroFilme("Suspense"));
+    }
+}
