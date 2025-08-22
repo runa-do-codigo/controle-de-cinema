891763d085f807d7afd8160d8c7b2b9c7be9b9eb
Iagodizlla <iagoschlemper@gmail.com>
2025-08-22 14:52:35 -0300

    Implementado Testes de Filme em Interface

---
+++ ControleDeCinema.Testes.Interface/ModuloFilme/FilmeFormPageObject.cs
@@ -0,0 +1,37 @@
+﻿using OpenQA.Selenium;
+using OpenQA.Selenium.Support.UI;
+
+namespace ControleDeCinema.Testes.Interface.ModuloGeneroFilme;
+
+public class FilmeFormPageObject
+{
+    private readonly IWebDriver driver;
+    private readonly WebDriverWait wait;
+
+    public FilmeFormPageObject(IWebDriver driver)
+    {
+        this.driver = driver;
+
+        wait = new WebDriverWait(driver, TimeSpan.FromSeconds(10));
+
+        wait.Until(d => d.FindElement(By.CssSelector("form")).Displayed);
+    }
+
+    public FilmeFormPageObject PreencherNome(string nome)
+    {
+        var inputNome = driver?.FindElement(By.Id("Nome"));
+        inputNome?.Clear();
+        inputNome?.SendKeys(nome);
+
+        return this;
+    }
+
+    public FilmeIndexPageObject Confirmar()
+    {
+        wait.Until(d => d.FindElement(By.CssSelector("button[type='submit']"))).Click();
+
+        wait.Until(d => d.FindElement(By.CssSelector("a[data-se='btnCadastrar']")).Displayed);
+
+        return new FilmeIndexPageObject(driver!);
+    }
+}
---
+++ ControleDeCinema.Testes.Interface/ModuloFilme/FilmeInterfaceTests.cs
@@ -0,0 +1,68 @@
+﻿using TesteFacil.Testes.Interface.Compartilhado;
+
+namespace ControleDeCinema.Testes.Interface.ModuloGeneroFilme;
+
+[TestClass]
+[TestCategory("Tests de Interface de Filme")]
+public sealed class FilmeInterfaceTests : TestFixture
+{
+    [TestMethod]
+    public void Deve_Cadastrar_Filme_Corretamente()
+    {
+        // Arange
+        var indexPageObject = new FilmeIndexPageObject(driver!)
+            .IrPara(enderecoBase!);
+
+        // Act
+        indexPageObject
+            .ClickCadastrar()
+            .PreencherNome("O Mano")
+            .Confirmar();
+
+        // Assert
+        Assert.IsTrue(indexPageObject.ContemFilme("O Mano"));
+    }
+
+    [TestMethod]
+    public void Deve_Editar_Filme_Corretamente()
+    {
+        // Arrange
+        var indexPageObject = new FilmeIndexPageObject(driver!)
+            .IrPara(enderecoBase!);
+
+        indexPageObject
+            .ClickCadastrar()
+            .PreencherNome("O Mano")
+            .Confirmar();
+
+        // Act
+        indexPageObject
+            .ClickEditar()
+            .PreencherNome("O Mano Editada")
+            .Confirmar();
+
+        // Assert
+        Assert.IsTrue(indexPageObject.ContemFilme("O Mano Editada"));
+    }
+
+    [TestMethod]
+    public void Deve_Excluir_Filme_Corretamente()
+    {
+        // Arrange
+        var indexPageObject = new FilmeIndexPageObject(driver!)
+            .IrPara(enderecoBase!);
+
+        indexPageObject
+            .ClickCadastrar()
+            .PreencherNome("O Mano")
+            .Confirmar();
+
+        // Act
+        indexPageObject
+            .ClickExcluir()
+            .Confirmar();
+
+        // Assert
+        Assert.IsFalse(indexPageObject.ContemFilme("O Mano"));
+    }
+}
