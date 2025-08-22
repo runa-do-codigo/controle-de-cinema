907cf7217ad5ad46ecec6300284373b382ff8755
Iagodizlla <iagoschlemper@gmail.com>
2025-08-22 14:45:29 -0300

    Implementado Testes de Sala em Interface

---
+++ ControleDeCinema.Testes.Interface/ModuloSala/SalaFormPageObject.cs
@@ -0,0 +1,37 @@
+﻿using OpenQA.Selenium;
+using OpenQA.Selenium.Support.UI;
+
+namespace ControleDeCinema.Testes.Interface.ModuloGeneroFilme;
+
+public class SalaFormPageObject
+{
+    private readonly IWebDriver driver;
+    private readonly WebDriverWait wait;
+
+    public SalaFormPageObject(IWebDriver driver)
+    {
+        this.driver = driver;
+
+        wait = new WebDriverWait(driver, TimeSpan.FromSeconds(10));
+
+        wait.Until(d => d.FindElement(By.CssSelector("form")).Displayed);
+    }
+
+    public SalaFormPageObject PreencherNome(string nome)
+    {
+        var inputNome = driver?.FindElement(By.Id("Nome"));
+        inputNome?.Clear();
+        inputNome?.SendKeys(nome);
+
+        return this;
+    }
+
+    public SalaIndexPageObject Confirmar()
+    {
+        wait.Until(d => d.FindElement(By.CssSelector("button[type='submit']"))).Click();
+
+        wait.Until(d => d.FindElement(By.CssSelector("a[data-se='btnCadastrar']")).Displayed);
+
+        return new SalaIndexPageObject(driver!);
+    }
+}
