6be613e60e09e1059d6935c8b94772e7a9ab4808
Iagodizlla <iagoschlemper@gmail.com>
2025-08-26 14:48:57 -0300

    Implementado Testes de Genero de Filme em Unidade

--- ControleDeCinema.Testes.Unidade/ModuloGeneroFilme/GeneroFilmeAppServiceTests.cs
+++ ControleDeCinema.Testes.Unidade/ModuloGeneroFilme/GeneroFilmeAppServiceTests.cs
@@ -1,7 +1,113 @@
-﻿namespace ControleDeCinema.Testes.Unidade.ModuloGeneroFilme;
+﻿using Moq;
+using ControleDeCinema.Dominio.ModuloGeneroFilme;
+using ControleDeCinema.Dominio.ModuloFilme;
+using ControledeCinema.Dominio.Compartilhado;
+using ControleDeCinema.Aplicacao.ModuloGeneroFilme;
+using Microsoft.Extensions.Logging;
+using ControleDeCinema.Dominio.ModuloAutenticacao;
+
+namespace ControleDeCinema.Testes.Unidade.ModuloGeneroFilme;
 
 [TestClass]
 [TestCategory("Testes de Unidade de Genero de Filme")]
 public sealed class GeneroFilmeAppServiceTests
 {
+    private Mock<ITenantProvider>? tenantProviderMock;
+    private Mock<IRepositorioGeneroFilme>? repositorioGeneroFilmeMock;
+    private Mock<IRepositorioFilme>? repositorioFilmeMock;
+    private Mock<IUnitOfWork>? unitOfWorkMock;
+    private Mock<ILogger<GeneroFilmeAppService>>? loggerMock;
+
+    private GeneroFilmeAppService? generoFilmeAppService;
+
+    [TestInitialize]
+    public void Setup()
+    {
+        tenantProviderMock = new Mock<ITenantProvider>();
+        repositorioGeneroFilmeMock = new Mock<IRepositorioGeneroFilme>();
+        unitOfWorkMock = new Mock<IUnitOfWork>();
+        loggerMock = new Mock<ILogger<GeneroFilmeAppService>>();
+        generoFilmeAppService = new GeneroFilmeAppService(
+            tenantProviderMock.Object,
+            repositorioGeneroFilmeMock.Object,
+            unitOfWorkMock.Object,
+            loggerMock.Object);
+    }
+
+    [TestMethod]
+    public void Cadastrar_Deve_Retornar_Ok_Quando_Genero_Filme_For_Valida()
+    {
+        // Arrange
+        var generoFilme = new GeneroFilme("Suspense");
+
+        var generoFilmeTeste = new GeneroFilme("Teste");
+
+        repositorioGeneroFilmeMock?
+            .Setup(r => r.SelecionarRegistros())
+            .Returns(new List<GeneroFilme>() { generoFilmeTeste });
+
+        // Act
+        var resultado = generoFilmeAppService?.Cadastrar(generoFilme);
+
+        // Assert
+        repositorioGeneroFilmeMock?.Verify(r => r.Cadastrar(generoFilme), Times.Once);
+
+        unitOfWorkMock?.Verify(u => u.Commit(), Times.Once);
+
+        Assert.IsNotNull(resultado);
+        Assert.IsTrue(resultado.IsSuccess);
+    }
+
+    [TestMethod]
+    public void Cadastrar_Deve_Retornar_Falha_Quando_Genero_Filme_For_Duplicado()
+    {
+        // Arrange
+        var generoFilme = new GeneroFilme("Suspense");
+
+        var generoFilmeTeste = new GeneroFilme("Suspense");
+
+        repositorioGeneroFilmeMock?
+            .Setup(r => r.SelecionarRegistros())
+            .Returns(new List<GeneroFilme>() { generoFilmeTeste });
+
+        // Act
+        var resultado = generoFilmeAppService?.Cadastrar(generoFilme);
+
+        // Assert
+        repositorioGeneroFilmeMock?.Verify(r => r.Cadastrar(generoFilme), Times.Never);
+
+        unitOfWorkMock?.Verify(u => u.Commit(), Times.Never);
+
+        Assert.IsNotNull(resultado);
+        Assert.IsTrue(resultado.IsFailed);
+    }
+
+    [TestMethod]
+    public void Cadastrar_Deve_Retornar_Falha_Quando_Excecao_For_Lancada()
+    {
+        // Arrange
+        var generoFilme = new GeneroFilme("Suspense");
+
+        repositorioGeneroFilmeMock?
+            .Setup(r => r.SelecionarRegistros())
+            .Returns(new List<GeneroFilme>());
+
+        unitOfWorkMock?
+            .Setup(r => r.Commit())
+            .Throws(new Exception("Erro inesperado"));
+
+        // Act
+        var resultado = generoFilmeAppService?.Cadastrar(generoFilme);
+
+        // Assert
+        unitOfWorkMock?.Verify(u => u.Rollback(), Times.Once);
+
+        Assert.IsNotNull(resultado);
+
+        var mensagemErro = resultado.Errors.First().Message;
+
+        Assert.AreEqual("Ocorreu um erro interno do servidor", mensagemErro);
+
+        Assert.IsTrue(resultado.IsFailed);
+    }
 }
--- ControleDeCinema.Testes.Unidade/ModuloGeneroFilme/GeneroFilmeTests.cs
+++ ControleDeCinema.Testes.Unidade/ModuloGeneroFilme/GeneroFilmeTests.cs
@@ -1,7 +1,47 @@
-﻿namespace ControleDeCinema.Testes.Unidade.ModuloGeneroFilme;
+﻿using ControleDeCinema.Dominio.ModuloGeneroFilme;
+
+namespace ControleDeCinema.Testes.Unidade.ModuloGeneroFilme;
 
 [TestClass]
 [TestCategory("Testes de Unidade de Genero de Filme")]
 public sealed class GeneroFilmeTests
 {
+    private GeneroFilme? generoFilme;
+
+    [TestMethod]
+    public void DeveCriarGeneroFilme()
+    {
+        // Arrange
+        string descricao = "Ação";
+        // Act
+        generoFilme = new GeneroFilme(descricao);
+        // Assert
+        Assert.IsNotNull(generoFilme);
+        Assert.AreEqual(descricao, generoFilme.Descricao);
+        Assert.AreNotEqual(Guid.Empty, generoFilme.Id);
+    }
+
+    [TestMethod]
+    public void DeveAtualizarGeneroFilme()
+    {
+        // Arrange
+        string descricaoInicial = "Ação";
+        string descricaoAtualizada = "Comédia";
+        generoFilme = new GeneroFilme(descricaoInicial);
+        var generoFilmeEditado = new GeneroFilme(descricaoAtualizada) { Id = generoFilme.Id };
+        // Act
+        generoFilme.AtualizarRegistro(generoFilmeEditado);
+        // Assert
+        Assert.AreEqual(descricaoAtualizada, generoFilme.Descricao);
+    }
+
+    [TestMethod]
+    public void DeveTerListaDeFilmesInicializada()
+    {
+        // Arrange & Act
+        generoFilme = new GeneroFilme("Drama");
+        // Assert
+        Assert.IsNotNull(generoFilme.Filmes);
+        Assert.AreEqual(0, generoFilme.Filmes.Count);
+    }
 }
