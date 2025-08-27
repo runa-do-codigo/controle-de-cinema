fea1ae117f28e5eed0d6cf7b27182d570f1b9718
Iagodizlla <iagoschlemper@gmail.com>
2025-08-27 14:20:37 -0300

    Implementado Testes de Filme em Unidade

---
+++ ControleDeCinema.Testes.Unidade/ModuloFilme/FilmeAppServiceTests.cs
@@ -0,0 +1,114 @@
+﻿using ControledeCinema.Dominio.Compartilhado;
+using ControleDeCinema.Aplicacao.ModuloFilme;
+using ControleDeCinema.Aplicacao.ModuloGeneroFilme;
+using ControleDeCinema.Dominio.ModuloAutenticacao;
+using ControleDeCinema.Dominio.ModuloFilme;
+using ControleDeCinema.Dominio.ModuloGeneroFilme;
+using Microsoft.Extensions.Logging;
+using Moq;
+
+namespace ControleDeCinema.Testes.Unidade.ModuloFilme;
+
+[TestClass]
+[TestCategory("Testes de Unidade de Filme")]
+public sealed class FilmeAppServiceTests
+{
+    private Mock<ITenantProvider>? tenantProviderMock;
+    private Mock<IRepositorioGeneroFilme>? repositorioGeneroFilmeMock;
+    private Mock<IRepositorioFilme>? repositorioFilmeMock;
+    private Mock<IUnitOfWork>? unitOfWorkMock;
+    private Mock<ILogger<FilmeAppService>>? loggerMock;
+
+    private FilmeAppService? filmeAppService;
+
+    [TestInitialize]
+    public void Setup()
+    {
+        tenantProviderMock = new Mock<ITenantProvider>();
+        repositorioGeneroFilmeMock = new Mock<IRepositorioGeneroFilme>();
+        unitOfWorkMock = new Mock<IUnitOfWork>();
+        loggerMock = new Mock<ILogger<FilmeAppService>>();
+        filmeAppService = new FilmeAppService(
+            tenantProviderMock.Object,
+            repositorioFilmeMock.Object,
+            unitOfWorkMock.Object,
+            loggerMock.Object);
+    }
+
+    [TestMethod]
+    public void Cadastrar_DeveRetornarOk_QuandoFilmeForValida()
+    {
+        // Arrange
+        var filme = new Filme("O Mano", 129, true, new GeneroFilme("Susupense"));
+
+        var filmeTeste = new Filme("Teste", 129, true, new GeneroFilme("Susupense"));
+
+        repositorioFilmeMock?
+            .Setup(r => r.SelecionarRegistros())
+            .Returns(new List<Filme>() { filmeTeste });
+
+        // Act
+        var resultado = filmeAppService?.Cadastrar(filme);
+
+        // Assert
+        repositorioFilmeMock?.Verify(r => r.Cadastrar(filme), Times.Once);
+
+        unitOfWorkMock?.Verify(u => u.Commit(), Times.Once);
+
+        Assert.IsNotNull(resultado);
+        Assert.IsTrue(resultado.IsSuccess);
+    }
+
+    [TestMethod]
+    public void Cadastrar_DeveRetornarFalha_QuandoFilmeForDuplicada()
+    {
+        // Arrange
+        var filme = new Filme("O Mano", 129, true, new GeneroFilme("Susupense"));
+
+        var filmeTeste = new Filme("O Mano", 129, true, new GeneroFilme("Susupense"));
+
+        repositorioFilmeMock?
+            .Setup(r => r.SelecionarRegistros())
+            .Returns(new List<Filme>() { filmeTeste });
+
+        // Act
+        var resultado = filmeAppService?.Cadastrar(filme);
+
+        // Assert
+        repositorioFilmeMock?.Verify(r => r.Cadastrar(filme), Times.Never);
+
+        unitOfWorkMock?.Verify(u => u.Commit(), Times.Never);
+
+        Assert.IsNotNull(resultado);
+        Assert.IsTrue(resultado.IsFailed);
+    }
+
+    [TestMethod]
+    public void Cadastrar_DeveRetornarFalha_QuandoExcecaoForLancada()
+    {
+        // Arrange
+        var filme = new Filme("O Mano", 129, true, new GeneroFilme("Susupense"));
+
+        repositorioFilmeMock?
+            .Setup(r => r.SelecionarRegistros())
+            .Returns(new List<Filme>());
+
+        unitOfWorkMock?
+            .Setup(r => r.Commit())
+            .Throws(new Exception("Erro inesperado"));
+
+        // Act
+        var resultado = filmeAppService?.Cadastrar(filme);
+
+        // Assert
+        unitOfWorkMock?.Verify(u => u.Rollback(), Times.Once);
+
+        Assert.IsNotNull(resultado);
+
+        var mensagemErro = resultado.Errors.First().Message;
+
+        Assert.AreEqual("Ocorreu um erro interno do servidor", mensagemErro);
+
+        Assert.IsTrue(resultado.IsFailed);
+    }
+}
---
+++ ControleDeCinema.Testes.Unidade/ModuloFilme/FilmeTests.cs
@@ -0,0 +1,55 @@
+﻿using ControleDeCinema.Dominio.ModuloFilme;
+using ControleDeCinema.Dominio.ModuloGeneroFilme;
+
+namespace ControleDeCinema.Testes.Unidade.ModuloFilme;
+
+[TestClass]
+[TestCategory("Testes de Unidade de FIlme")]
+public sealed class FilmeTests
+{
+    private Filme? filme;
+
+    [TestMethod]
+    public void DeveCriarGeneroFilme()
+    {
+        // Arrange
+        string titulo = "O Mano";
+        int duracao = 129;
+        bool lancamento = true;
+        var generoFilme = new GeneroFilme("Susupense");
+        // Act
+        filme = new Filme(titulo, duracao, lancamento, generoFilme);
+        // Assert
+        Assert.IsNotNull(filme);
+        Assert.AreEqual(titulo, filme.Titulo);
+        Assert.AreEqual(duracao, filme.Duracao);
+        Assert.AreEqual(lancamento, filme.Lancamento);
+        Assert.AreEqual(generoFilme, filme.Genero);
+        Assert.AreNotEqual(Guid.Empty, generoFilme.Id);
+    }
+
+    [TestMethod]
+    public void DeveAtualizarFilme()
+    {
+        // Arrange
+        string titulo = "O Mano";
+        int duracao = 129;
+        bool lancamento = true;
+        var generoFilme = new GeneroFilme("Susupense");
+
+        string tituloAtualizado = "O Mano 2";
+        int duracaoAtualizado = 109;
+        bool lancamentoAtualizado = false;
+        var generoFilmeAtualizado = new GeneroFilme("Terror");
+
+        var filme = new Filme(titulo, duracao, lancamento, generoFilme);
+        var filmeEditado = new Filme(tituloAtualizado, duracaoAtualizado, lancamentoAtualizado, generoFilmeAtualizado) { Id = filme.Id };
+        // Act
+        filme.AtualizarRegistro(filmeEditado);
+        // Assert
+        Assert.AreEqual(tituloAtualizado, filme.Titulo);
+        Assert.AreEqual(duracaoAtualizado, filme.Duracao);
+        Assert.AreEqual(lancamentoAtualizado, filme.Lancamento);
+        Assert.AreEqual(generoFilmeAtualizado, filme.Genero);
+    }
+}
